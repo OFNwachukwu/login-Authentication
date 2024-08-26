1ddb23769965c28e92f479530429b4fad70a4de6
OFNwachukwu <nwachukwuokudo58@gmil.com>
2024-08-26 12:06:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form');
+const firstname = document.getElementById('first-name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirm_password = document.getElementById('confirm-password');
+
+form.addEventListener('submit', (e) => {
+    e.default
+} ); 
